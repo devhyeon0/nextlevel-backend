917b1f92b5663e2cacbdc2ee205daa34b27b2941
devhyeon0 <devhyeon0@gmail.com>
2024-02-15 16:42:09 +0900

    first commit

---
+++ src/main/java/com/nextlevel/NextlevelApplication.java
@@ -0,0 +1,13 @@
+package com.nextlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextlevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextlevelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextlevel/NextlevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextlevelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
