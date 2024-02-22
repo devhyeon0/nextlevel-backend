0d81c0743ca15c39d418c82acc21dc1891f5d8fc
devhyeon0 <devhyeon0@gmail.com>
2024-02-22 19:28:50 +0900

    feat: 유효성 검증 추가

--- src/main/java/com/nextlevel/post/dto/PostRequestDto.java
+++ src/main/java/com/nextlevel/post/dto/PostRequestDto.java
@@ -1,6 +1,8 @@
 package com.nextlevel.post.dto;
 
 import com.nextlevel.post.entity.PostStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +10,10 @@ import lombok.Getter;
 @Builder
 public class PostRequestDto {
 
+    @NotBlank
     private String title;
+
+    @NotNull
     private String content;
     private Long views;
     private PostStatus status;
