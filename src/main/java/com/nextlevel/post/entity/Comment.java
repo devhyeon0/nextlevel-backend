d2797fdb3a0cff4a67cd6d8d5ed1555a19768e02
devhyeon0 <devhyeon0@gmail.com>
2024-02-23 17:51:41 +0900

    feat: comment entity 구현

---
+++ src/main/java/com/nextlevel/post/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.nextlevel.post.entity;
+
+import com.nextlevel.common.audit.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "comments")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "commnet_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+}
